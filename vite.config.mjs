29231e8edb8d5ea19d7f93695fb195717bedda72
Anaid0616 <152166423+Anaid0616@users.noreply.github.com>
2025-02-06 17:04:20 +0100

    vite configuration

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   appType: 'mpa',
-  base: process.env.NODE_ENV === 'production' ? '/repository-name/' : '',
+  base: process.env.NODE_ENV === 'production' ? '/semester-project2/' : '',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
