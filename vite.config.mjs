856d443c9251f5050fca15755a5ff6f61b7cc1e6
Anaid0616 <152166423+Anaid0616@users.noreply.github.com>
2025-02-06 17:01:47 +0100

    vite configuration

--- vite.config.mjs
+++ vite.config.mjs
@@ -14,8 +14,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, './index.html'),
-        login: resolve(__dirname, './auth/login/index.html'),
-        register: resolve(__dirname, './auth/register/index.html'),
+        login: resolve(__dirname, './login/index.html'),
+        register: resolve(__dirname, './register/index.html'),
         profile: resolve(__dirname, './profile/index.html'),
         listing: resolve(__dirname, './listing/index.html'),
         editListing: resolve(__dirname, './listing/edit/index.html'),
